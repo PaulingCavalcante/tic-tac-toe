85873c570e201aa157544933da1267b7aae9e24d
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-10-31 18:48:44 -0300

    feat: new style and features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Game from './components/Game';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,23 @@
 // src/components/Game.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 import Board from './Board';
+import calculateWinner from '../utils/calculateWinner';
 
 function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [showToast, setShowToast] = useState(false);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
+  const winner = calculateWinner(currentSquares);
+
+  useEffect(() => {
+    if (winner) {
+      setShowToast(true);
+    }
+  }, [winner]);
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
@@ -16,6 +27,7 @@ function Game() {
 
   function jumpTo(move) {
     setCurrentMove(move);
+    setShowToast(false); // Fecha o toast ao voltar para um movimento anterior
   }
 
   const moves = history.map((_, move) => (
@@ -31,9 +43,28 @@ function Game() {
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div className="game-info">
+      {/* <div className="game-info">
         <ol>{moves}</ol>
-      </div>
+      </div> */}
+      
+      <ToastContainer position="top-end" className="p-3" style={{ zIndex: 1 }}>
+        <Toast
+          show={showToast}
+          onClose={() => setShowToast(false)}
+          delay={3000}
+          autohide
+          bg="dark"
+        >
+          <Toast.Header closeButton={false} className='bg-dark text-white'>
+            <img src="" className="rounded me-2" alt="" />
+            <strong className="me-auto text-white">Game Over</strong>
+            <small className="text-muted">just now</small>
+          </Toast.Header>
+          <Toast.Body className="text-white">
+            Congratulations! {winner} is the winner!
+          </Toast.Body>
+        </Toast>
+      </ToastContainer>
     </div>
   );
 }
