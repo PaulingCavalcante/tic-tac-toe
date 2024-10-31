1fc128214f45f91636a4c21cbbc6e6040489c29c
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-10-31 17:12:46 -0300

    refactor: separate the components and utils

---
+++ src/components/Board.jsx
@@ -0,0 +1,40 @@
+// src/components/Board.jsx
+import React from 'react';
+import Square from './Square';
+import calculateWinner from '../utils/calculateWinner';
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) return;
+
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {[0, 1, 2].map((i) => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+      <div className="board-row">
+        {[3, 4, 5].map((i) => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+      <div className="board-row">
+        {[6, 7, 8].map((i) => (
+          <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Board;
---
+++ src/components/Game.jsx
@@ -0,0 +1,41 @@
+// src/components/Game.jsx
+import React, { useState } from 'react';
+import Board from './Board';
+
+function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+  }
+
+  const moves = history.map((_, move) => (
+    <li key={move}>
+      <button onClick={() => jumpTo(move)}>
+        {move ? `Go to move #${move}` : 'Go to game start'}
+      </button>
+    </li>
+  ));
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
